6a9165fec1db06228e1d55459dd9e8a00788deaa
Justin <justinms@outlook.com>
2020-07-05 14:30:09 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let hearts = document.getElementsByClassName('like-glyph')
 
+for (const heart of hearts){
+  heart.addEventListener('click', function(event){
 
+    mimicServerCall().then(function(response){
+      clickedHeart = event.target
+
+      if (clickedHeart.getAttribute("class")){
+        clickedHeart.removeAttribute("class")
+        clickedHeart.innerText = EMPTY_HEART
+      }
+      else {
+        clickedHeart.setAttribute("class", "activated-heart")
+        clickedHeart.innerText = FULL_HEART
+      }
+      
+      return response.json();
+
+    }).catch(function(error){
+      const modal = document.getElementById('modal');
+      modal.setAttribute("class", "display");
+
+      const modalMessage = document.getElementById('modal-message');
+      modalMessage.innerText = error.message;
+
+      setTimeout(function(){modal.setAttribute("class", "hidden")}, 1000)
+    })
+  })
+}
 
 
 //------------------------------------------------------------------------------
