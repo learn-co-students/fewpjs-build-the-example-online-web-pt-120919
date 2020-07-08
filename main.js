32584fd28ee085e275c5246b91c1ea8846328f58
Robert Shields <robert.shieldsiv@gmail.com>
2020-07-08 15:46:55 -0400

    Done.

--- main.js
+++ main.js
@@ -3,23 +3,51 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelector('#modal').className = "hidden"
 
+let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+};
 
+let colorStates = {
+    "red": "",
+    "": "red"
+};
 
+let articleHearts = document.querySelectorAll(".like");
+
+function likeCallback(e) {
+    let heart = e.target;
+    mimicServerCall("bogusUrl")
+        .then(function(serverMessage) {
+            heart.innerText = glyphStates[heart.innerText];
+            heart.style.color = colorStates[heart.style.color];
+        })
+        .catch(function(error) {
+            document.getElementById("modal").className = "";
+        });
+}
+
+for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+}
+
+// struggled with this one so I had to look up to the solution -- to refactor
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            let isRandomFailure = Math.random() < .2
+            if (isRandomFailure) {
+                reject("Random server error. Try again.");
+            } else {
+                resolve("Pretend remote server notified of action!");
+            }
+        }, 300);
+    });
 }
