f76698e22985ec96a70245243c790ee8f0a04880
cep104 <candicelizabethpeters@gmail.com>
2020-07-08 23:49:16 -0500

    Done.

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
   
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 document.addEventListener('DOMContentLoaded', (event) => {
-  console.log(EMPTY_HEART);
-  mimicServerCall()
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
+  let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  let colorStates = {
+    "red" : "",
+    "": "red"
+  };
+
+  let articleHearts = document.querySelectorAll(".like");
+
+  function likeCallback(e) {
+    let heart = e.target
+  mimicServerCall('url')
+  .then(function(serverMessage){
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
   })
   .catch(function(error) {
     alert(error.message);
-    console.log(error.message);
     let errorMessageHTML = document.getElementById('modal')
     errorMessageHTML.style.visibility = "visible";
     setTimeout(function(){ errorMessageHTML.style.visibility = "hidden"; }, 5000);
   });
+}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 })
 
 
