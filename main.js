8b6670dfcc9fe1f0faea9c4b5ac57c319bcc9ba6
cep104 <candicelizabethpeters@gmail.com>
2020-07-08 23:15:42 -0500

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+  
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+document.addEventListener('DOMContentLoaded', (event) => {
+  console.log(EMPTY_HEART);
+  mimicServerCall()
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(function(error) {
+    alert(error.message);
+    console.log(error.message);
+    let errorMessageHTML = document.getElementById('modal')
+    errorMessageHTML.style.visibility = "visible";
+    setTimeout(function(){ errorMessageHTML.style.visibility = "hidden"; }, 5000);
+  });
+})
 
 
 
