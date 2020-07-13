d468dde0496c986c3abd33bbabc161d33a0fdf85
SMakaiTakori <makait87@icloud.com>
2020-07-13 14:30:24 -0400

    Done.

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal')
+modal.className = 'hidden'
 
+const hearts = document.getElementsByClassName('like-glyph')
 
+  for (heart of hearts) {
+    heart.addEventListener("click", function(e) {
+      
+      mimicServerCall()
+    .then(resp => {
+      if (e.target.innerHTML === EMPTY_HEART) {
+        e.target.innerHTML = FULL_HEART
+        e.target.className = "activated-heart"  
+      }
+      else {
+        e.target.innerHTML = EMPTY_HEART
+        e.target.className = "like-glyph"  
+      }    
+    })
+    .catch(error => {
+      modal.className = ""
+      const modalMessage = document.querySelector('#modal-message')
+      modalMessage.innerText = error
+        setTimeout(() => {
+          modal.className = "hidden"
+        },5000)
+        })
+    })
+
+    }
+  
+
+  
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
@@ -19,7 +50,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
         reject("Random server error. Try again.");
       } else {
         resolve("Pretend remote server notified of action!");
-      }
+      }          
     }, 300);
   });
 }
