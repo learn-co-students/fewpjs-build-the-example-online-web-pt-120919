7c518c60bcc1733e02dbb298fdcc2bdd13b867a7
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-07-18 15:10:04 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal")
+modal.className = "hidden"
 
+const hearts = document.getElementsByClassName("like-glyph")
+for (heart of hearts) {
+  heart.addEventListener("click", (e) => {
+    mimicServerCall()
+    .then(() => {
+      if (e.target.innerHTML === EMPTY_HEART) {
+        e.target.innerHTML = FULL_HEART
+        e.target.className = "activated-heart"
+      } else {
+        e.target.innerHTML = EMPTY_HEART
+        e.target.className = "like-glyph"
+      }
+    }
+    )
+    .catch((error) => {
+      modal.className = ""
+      const modalMessage = document.querySelector("#modal-message")
+      modalMessage.innerHTML = error
+      setTimeout(() => {
+        modal.className = "hidden"
+      }, 5000);
+    })
+  })
+}
 
 
 //------------------------------------------------------------------------------
